7a9fabe9ca7288fd0fc6b6d66ac14f5b759dbdb9
Mariana Ferreira <mariana.ballardin@hotmail.com>
2022-09-13 19:27:38 +0300

    refatoração

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-const randomNumber = Math.round(Math.random()*10)
+const { handle } = require("express/lib/application")
 
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const randomNumber = Math.round(Math.random()*10)
 let xAttempts = 1
 
-function handleClick(event) {
+//eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e){
+  if(e.key == 'Enter') {
+    handleResetClick()
+
+  }
+})
+
+function handleTryClick(event) {
 event.preventDefault()
 
 const inputNumber = document.querySelector('#inputNumber')
@@ -11,13 +26,13 @@ const inputNumber = document.querySelector('#inputNumber')
 // console.log(inputNumber.value)
 
 if(Number(inputNumber.value) == randomNumber) {
-  document.querySelector(".screen1").classList.add("hide")
-  document.querySelector(".screen2").classList.remove("hide")
+  toggleScreen()
+  
 
-  document.querySelector(".screen2 h2").innerText = `Você acertou em ${xAttempts} tentativas!`
+  screen2.querySelector("h2").innerText = `Você acertou em ${xAttempts} tentativas!`
  
 }
-
+inputNumber.value = ""
 xAttempts++
 
 }
@@ -41,3 +56,16 @@ xAttempts++
 
 //   alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você acertou em apenas 1 tentativa!`)
 // }
+
+
+
+function toggleScreen(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+
+}
+
+function handleResetClick(){
+  toggleScreen()
+  xAttempts = 1
+}
